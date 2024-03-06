f7a98fc759fd11d7457f2967427dad2d0caacdd3
AhmadAdit <nugrohoadit020@gmail.com>
2024-03-06 12:46:46 +0700

    db

---
+++ db_restoran.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 23, 2024 at 08:17 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_restoran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masakan`
+--
+
+CREATE TABLE `masakan` (
+  `id_masakan` int(11) NOT NULL,
+  `nama_masakan` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `masakan`
+--
+
+INSERT INTO `masakan` (`id_masakan`, `nama_masakan`, `harga`, `status`) VALUES
+(4, 'Nasi goreng', 12000, 'Tersedia'),
+(5, 'Nasi padang ', 15000, 'Tersedia'),
+(6, 'Mie goreng', 8000, 'Tersedia'),
+(9, 'Ayam goreng', 17000, 'Tersedia'),
+(10, 'Ayam bakar', 9000, 'Tersedia'),
+(11, 'Nila goreng', 24000, 'Tersedia'),
+(13, 'Cah kangkung', 15000, 'Tersedia'),
+(14, 'Kepiting saus padang', 55000, 'Tersedia'),
+(15, 'udang saos telur asin', 45000, 'Tersedia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `nama_pelanggan` varchar(30) NOT NULL,
+  `id_masakan` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_masakan` varchar(30) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `total_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `nama_pelanggan`, `id_masakan`, `tanggal`, `nama_masakan`, `harga`, `jumlah_beli`, `total_bayar`) VALUES
+(3, '', 6, '2024-02-11', 'Mie goreng', 8000, 2, 16000),
+(5, 'Bagas', 4, '2024-02-22', 'Nasi goreng', 12000, 1, 12000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_user`, `id_level`) VALUES
+(1, 'Admin', 'Admin123', 'aman', 1),
+(6, 'Owner', 'owner123', 'yati', 1),
+(7, 'Pelanggan', 'pelanggan123', 'suroto', 4),
+(8, 'Kasir', 'kasir123', 'mistri', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `masakan`
+--
+ALTER TABLE `masakan`
+  ADD PRIMARY KEY (`id_masakan`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_masakan` (`id_masakan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `masakan`
+--
+ALTER TABLE `masakan`
+  MODIFY `id_masakan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_masakan`) REFERENCES `masakan` (`id_masakan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
